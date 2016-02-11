d87a01a334dcc67ab473b2aaffe62bca224e9d0f
Alino <cirhuzalain@gmail.com>
2016-02-11 12:25:10 +0200

    Add map.js

---
+++ static/js/map.js
@@ -0,0 +1,16 @@
+function initialize() {
+  var myLatlng = new google.maps.LatLng(-1.9446696,30.089721);
+  var mapOptions = {
+    zoom: 14,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('klab-map'), mapOptions);
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'kLab',
+      icon: image
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
