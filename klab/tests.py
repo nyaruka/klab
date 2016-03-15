0a665dd3435fede3c5d578bbdc2637a154f2d99a
Alino <cirhuzalain@gmail.com>
2016-03-15 09:53:00 +0200

    start adding unit test for the main page

--- klab/tests.py
+++ klab/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from smartmin.tests import SmartminTest
+from klab.events.models import Event
+from klab.blog.models import Post
+from django.core.urlresolvers import reverse
 
 class KLabTest(SmartminTest):
     """
@@ -11,5 +14,29 @@ class KLabTest(SmartminTest):
 
         self.admin = self.create_user('admin', ["Administrator"])
         self.editor = self.create_user('editor', ["Editors"])
+        self.post = Post.objects.create(title="Apply for Global Entrepreneurship Summit",
+        								body="Join Entrepreneurs from all around the world",
+        								image_id="20919667939",
+        								created_by = self.editor,
+        								modified_by=self.editor)
+        self.event = Event.objects.create(title="",
+        									date="",
+        									time="",
+        									duration="",
+        									description="",
+        									venue="",
+        									logo="",
+        									description="",
+        									recurrence_type="",
+        									end_date="",
+        									photo_tag="",
+        									created_by=self.editor,
+        									modified_by=self.editor)
+
+    def test_main_page_with_post_event(self):
+    	response = self.client.get(reverse('public_home'))
+    	self.assertEqual(response.status_code,200)
+    	self.assertQuerysetEqual(response.context['recent'],
+    		['<Post: Apply for Global Entrepreneurship Summit>'])
         
 
